0eaa16f440f7f0417ec98e68bb94ffba0889e2ef
Sławek Kania <slawek.kania@gmail.com>
2026-06-17 21:29:17 +0200

    Created vovodship dictionary

---
+++ database/migrations/2026_06_17_192839_create_voivodships_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('voivodships', function (Blueprint $table) {
+            $table->id();
+
+            // Nazwa województwa
+            $table->string('name_pl'); // np. Śląskie
+            $table->string('name_en')->nullable(); // opcjonalnie na przyszłość
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('voivodships');
+    }
+};
