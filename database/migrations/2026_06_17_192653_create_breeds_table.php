12e85e5598e3da775a0d16436ef58a0db8762834
Sławek Kania <slawek.kania@gmail.com>
2026-06-17 21:27:43 +0200

    Created breeds dictionary

---
+++ database/migrations/2026_06_17_192653_create_breeds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('breeds', function (Blueprint $table) {
+            $table->id();
+
+            // powiązanie z gatunkiem
+            $table->unsignedBigInteger('species_id');
+
+            // nazwy rasy
+            $table->string('breed_pl'); // np. Owczarek niemiecki
+            $table->string('breed_en')->nullable(); // na przyszłość
+
+            $table->timestamps();
+
+            // klucz obcy (możesz dodać teraz lub później)
+            // $table->foreign('species_id')->references('id')->on('species');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('breeds');
+    }
+};
