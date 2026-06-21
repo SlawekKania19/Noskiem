b0454de6c2921475d4c05b670654573bc10ffac9
Sławek Kania <slawek.kania@gmail.com>
2026-06-21 16:09:58 +0200

    Limited access to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\AnimalEditController;
 use App\Http\Controllers\ModerationController;
@@ -14,22 +16,20 @@ use App\Http\Controllers\ModerationController;
 |--------------------------------------------------------------------------
 */
 
-// -------------------------------
-// PUBLICZNE API (czytanie ogłoszeń)
-// -------------------------------
-Route::get('/animals', [AnimalController::class, 'index']);
-Route::get('/animals/{animal}', [AnimalController::class, 'show']);
+Route::middleware('throttle:30,1')->group(function () {
 
-// -------------------------------
-// TWORZENIE I EDYCJE (pending)
-// -------------------------------
-Route::post('/animal-edits', [AnimalEditController::class, 'store']);
-Route::get('/animal-edits/pending', [AnimalEditController::class, 'indexPending']);
-Route::get('/animal-edits/{animalEdit}', [AnimalEditController::class, 'show']);
+    // PUBLICZNE API
+    Route::get('/animals', [AnimalController::class, 'index']);
+    Route::get('/animals/{animal}', [AnimalController::class, 'show']);
 
-// -------------------------------
-// MODERACJA
-// -------------------------------
-Route::post('/moderation/approve/{animalEdit}', [ModerationController::class, 'approve']);
-Route::post('/moderation/reject/{animalEdit}', [ModerationController::class, 'reject']);
-Route::get('/moderation/diff/{animalEdit}', [ModerationController::class, 'diff']);
+    // TWORZENIE I EDYCJE
+    Route::post('/animal-edits', [AnimalEditController::class, 'store']);
+    Route::get('/animal-edits/pending', [AnimalEditController::class, 'indexPending']);
+    Route::get('/animal-edits/{animalEdit}', [AnimalEditController::class, 'show']);
+
+    // MODERACJA
+    Route::post('/moderation/approve/{animalEdit}', [ModerationController::class, 'approve']);
+    Route::post('/moderation/reject/{animalEdit}', [ModerationController::class, 'reject']);
+    Route::get('/moderation/diff/{animalEdit}', [ModerationController::class, 'diff']);
+});
+
