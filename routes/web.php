a0c89b8bca417837b3bf8c1ec443ee158be3a7b6
Sławek Kania <slawek.kania@gmail.com>
2026-06-22 19:54:22 +0200

    Updated Animal Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,54 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\AnimalEditController;
+use App\Http\Controllers\ModerationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// ---------------------------
+// PUBLICZNE – tylko odczyt
+// ---------------------------
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/animals', [AnimalController::class, 'index'])
+    ->name('animals.index');
+
+Route::get('/animals/{animal}', [AnimalController::class, 'show'])
+    ->name('animals.show');
+
+
+// ---------------------------
+// TWORZENIE I EDYCJA – trafia do animal_edits
+// ---------------------------
+
+Route::get('/animals/create', [AnimalEditController::class, 'create'])
+    ->name('animals.create');
+
+Route::post('/animals', [AnimalEditController::class, 'store'])
+    ->name('animals.store');
+
+Route::get('/animals/{animal}/edit', [AnimalEditController::class, 'edit'])
+    ->name('animals.edit');
+
+Route::post('/animals/{animal}/edit', [AnimalEditController::class, 'update'])
+    ->name('animals.update');
+
+
+// ---------------------------
+// MODERACJA – zatwierdzanie zmian
+// ---------------------------
+
+Route::post('/moderation/{edit}/approve', [ModerationController::class, 'approve'])
+    ->name('moderation.approve');
+
+Route::post('/moderation/{edit}/reject', [ModerationController::class, 'reject'])
+    ->name('moderation.reject');
